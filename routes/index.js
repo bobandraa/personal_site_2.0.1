f52a869ee61ac04212d0c7c16c9e70075e9b3817
Sandra Cohen <sandra.cohen.ux@gmail.com>
2015-10-27 13:19:44 -0700

    Created smtp transport and post route slash email body. Also added form action route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer = require('nodemailer');
+
+/*create default SMTP transport*/
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'bobandraMailer@gmail.com',
+        pass: 'klasslkkk'
+    }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/*POST email body*/
+router.post('/email', function(req, res, next){
+    var from = req.body.email;
+    transporter.sendMail({
+      from: from,
+      to: 'sandra.cohen.ux@gmail.com',
+      subject:  from + " would like to contact you!",
+      text: req.body.message
+  }, function (err, body) {
+    console.log(err);
+    res.redirect("/");
+  });
+}); 
 
 module.exports = router;
