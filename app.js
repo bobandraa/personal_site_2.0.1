cf7ea8a3cbc82969891e1bf9532cc490152f473f
Sandra Cohen <sandra.cohen.ux@gmail.com>
2015-10-27 11:53:50 -0700

    Added mailer and Port to app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
+var port = process.env.PORT || 3000;
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
+var nodemailer = require("nodemailer");
+
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
