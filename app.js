7512813261662e00966f574b7ca5eca187c05608
Sandra Cohen <sandra.cohen.ux@gmail.com>
2015-10-27 13:25:22 -0700

    clean up comments and unused dependencies

--- app.js
+++ app.js
@@ -4,19 +4,13 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var nodemailer = require("nodemailer");
-
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-// var users = require('./routes/users');
-
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade')
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
@@ -31,7 +25,6 @@ app.use("/static", express.static(__dirname + '/static'));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
