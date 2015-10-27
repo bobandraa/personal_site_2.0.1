7eb717944e1fdf6bbd144579c52aea1c8d8e6775
Sandra Cohen <sandra.cohen.ux@gmail.com>
2015-10-27 13:18:18 -0700

    Removed user rout referances

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+// var users = require('./routes/users');
 
 var app = express();
 
@@ -31,7 +31,7 @@ app.use("/static", express.static(__dirname + '/static'));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
