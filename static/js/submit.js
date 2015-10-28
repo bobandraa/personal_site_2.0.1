eb681a49c8a175f97e0d77e2e43757dc588fc760
Sandra Cohen <sandra.cohen.ux@gmail.com>
2015-10-28 15:23:46 -0700

    updating to which method vs keyCode for cross-browser compatibility

--- static/js/submit.js
+++ static/js/submit.js
@@ -1,5 +1,5 @@
-$("#contact-form-box").keydown(function(event){
- if (event.keyCode == 13) {
+$("#contact-form-box").keypress(function(event){
+ if (event.which == 13) {
   $("#enter-submit").click();
  }
 });
